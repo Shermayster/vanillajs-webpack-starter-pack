2ed59454d84f6df76e8546eb1e1886b0d32425fd
pavelshermayster <sherm84@gmail.com>
2017-12-08 14:34:09 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,5 @@ module.exports = {
             "modules": true,
             "experimentalObjectRestSpread": true
         }
-    },
-    "rules": {
-        // enable additional rules
-
-        // override default options for rules from base configurations
-        "comma-dangle": ["error", "always"],
-        "no-cond-assign": ["error", "always"]
     }
 };

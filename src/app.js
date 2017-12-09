bb0872cb839f643a6dd67480abfe4f922923e3fa
pavelshermayster <sherm84@gmail.com>
2017-12-09 17:34:09 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -1,5 +1,2 @@
-import Test2 from './app2';
 
-const t2 = new Test2();
-t2.hello();
 
--- src/app2.js
+++
@@ -1,8 +0,0 @@
-export default class Test2 {
-    hello() {
-        console.log('hi');
-    }
-}
-
-const t = new Test2;
-// t.hello();
